814a37d3d823108ead809dacc0889347063f9028
David-A12 <santid121999@gmail.com>
2022-05-19 12:20:45 -0500

    Aqui se realizo un cambio al codigo

--- programa.c
+++ programa.c
@@ -1,4 +1,15 @@
-#include <stdio.h>
-main()
-	ṕrintf("hola mundo este es mi primer commit");
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=0,b=0,c=0;
+    cout<<"ingrese un numero: ";cin>>a;
+    cout<<"ingrese otro numero: ";cin>>b;
+    while(a<=b)
+    {
+        if(a%2!=0)
+        c=c+1; a=a+1;       
+    }
+    cout<<c<<endl;
+    cin.ignore(); return 0;
 }
